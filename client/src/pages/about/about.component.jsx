c628502efb169d0b60621231a278c2eb31243a7a
Nishant <nishantjain2503@gmail.com>
2023-08-22 22:58:14 +0530

    changed styling of about page

--- client/src/pages/about/about.component.jsx
+++ client/src/pages/about/about.component.jsx
@@ -1,32 +1,38 @@
-import React, { useEffect } from "react"
-import "./about.styles.scss"
+import React, { useEffect } from "react";
+import "./about.styles.scss";
 
 const About = () => {
-	useEffect(() => {
-		window.scrollTo(0, 0)
-	}, [])
-	return (
-		<div className="about-container">
-			<header>
-				<h1>About Us</h1>
-			</header>
-			<section className="grid">
-				<div className="images" id="first">
-					<p>Some images</p>
-				</div>
-				<div className="text">
-					<h2>Some title</h2>
-					<p>Lots of text</p>
-				</div>
-			</section>
-			<section className="grid">
-				<div className="text">
-					<p>Another paragraph</p>
-				</div>
-				<div className="images">Some more images</div>
-			</section>
-		</div>
-	)
-}
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div className="about-container">
+      
+      
+        
+        <header>
+        <h2>About Us</h2>
+      </header>
+      <div className="divider"></div>
+          <p id="body">
+            Hi there! My name is Kashish and I am the creator/owner of CandleLit
+            Company. I'm currently pursuing my Masters degree. I always utilised
+            my free time to think about what the next chapter of my life could
+            be.
 
-export default About
+<br/>
+<br/>
+			I developed a sudden passion for candle-making in May of 2021, and
+            haven't looked back since. I've transformed this little idea in my
+            head into this small business that I'm very passionate about, and am
+            so happy to share it with you all! Thank you for stopping by and for
+            your support. It means the world to me!
+			
+          </p>
+       
+      
+    </div>
+  );
+};
+
+export default About;
--- client/src/pages/product/product.component.jsx
+++ client/src/pages/product/product.component.jsx
@@ -1,30 +1,36 @@
-import React, { useState, useEffect, useContext } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { selectCartItems } from "../../store/cart/cart.selector"
-import { selectCurrentUser } from "../../store/user/user.selector"
+import React, { useState, useEffect, useContext } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectCartItems } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
-import { addItemToCart } from "../../store/cart/cart.action"
-import { useParams } from "react-router-dom"
-import Alert from "../../components/alert/alert.component"
-import Dropdown from "../../components/dropdown/dropdown.component"
-import { VscChevronUp, VscChevronDown, VscChromeClose } from "react-icons/vsc"
-import "./product.styles.scss"
+import { addItemToCart } from "../../store/cart/cart.action";
+import { useParams } from "react-router-dom";
+import Alert from "../../components/alert/alert.component";
+import Dropdown from "../../components/dropdown/dropdown.component";
+import {
+  VscChevronUp,
+  VscChevronDown,
+  VscChromeClose,
+  VscChromeMinimize,
+  VscAdd,
+} from "react-icons/vsc";
+import "./product.styles.scss";
 
-import { getProductById } from "../../utils/firebase/firebase.utils"
-import Button from "../../components/button/button.component"
+import { getProductById } from "../../utils/firebase/firebase.utils";
+import Button from "../../components/button/button.component";
 
 const Product = () => {
-	const { productId } = useParams()
-	const dispatch = useDispatch()
-	const cartItems = useSelector(selectCartItems)
-	const currentUser = useSelector(selectCurrentUser)
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+  const cartItems = useSelector(selectCartItems);
+  const currentUser = useSelector(selectCurrentUser);
 
-	const [productData, setProductData] = useState({})
-	const [viewImage, setViewImage] = useState(false)
-	const [quantity, setQuantity] = useState(1)
-	const [show, setShow] = useState(false)
-	const [isLoading, setIsLoading] = useState(true)
-	/* const IMAGE_LIST = {
+  const [productData, setProductData] = useState({});
+  const [viewImage, setViewImage] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+  const [show, setShow] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  /* const IMAGE_LIST = {
 		white:
 			"https://images.unsplash.com/photo-1608181831696-1f21b6e6e5e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
 		lavender:
@@ -34,151 +40,163 @@ const Product = () => {
 	}
 	const COLOR_LIST = ["white", "lavender", "beige"] */
 
-	const [productPhoto, setProductPhoto] = useState("")
+  const [productPhoto, setProductPhoto] = useState("");
+  const [showDetails, setShowDetails] = useState(false);
 
-	const handleQuantityChange = (e) => {
-		e.target.id === "add"
-			? setQuantity((quantity) => quantity + 1)
-			: quantity > 0 && setQuantity((quantity) => quantity - 1)
-	}
+  const handleQuantityChange = (e) => {
+    e.target.id === "add"
+      ? setQuantity((quantity) => quantity + 1)
+      : quantity > 0 && setQuantity((quantity) => quantity - 1);
+  };
 
-	useEffect(() => {
-		const getProductData = async () => {
-			const response = await getProductById(productId)
-			console.log(response)
-			setProductData({ ...response, id: productId })
-			setProductPhoto(response.images[0])
-			setIsLoading(false)
-		}
-		getProductData()
-	}, [])
+  useEffect(() => {
+    const getProductData = async () => {
+      const response = await getProductById(productId);
+      console.log(response);
+      setProductData({ ...response, id: productId });
+      setProductPhoto(response.images[0]);
+      setIsLoading(false);
+    };
+    getProductData();
+  }, []);
 
-	const addToCart = () => {
-		dispatch(addItemToCart(currentUser, cartItems, productData, quantity))
-		setShow(true)
-		setTimeout(() => {
-			setShow(false)
-			setQuantity(1)
-		}, 3000)
-	}
-	const { name, category, price, images, color, fragrance, weight } =
-		productData
+  const addToCart = () => {
+    dispatch(addItemToCart(currentUser, cartItems, productData, quantity));
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+      setQuantity(1);
+    }, 3000);
+  };
+  const { name, category, price, images, color, fragrance, weight } =
+    productData;
 
-	//change structure for images
-	/* const changeColor = (color) => {
+  //change structure for images
+  /* const changeColor = (color) => {
 		setProductColor(color)
 		setProductPhoto(images[color])
 	} */
-	const scrollUp = () => {
-		document.getElementById("row").scrollBy(0, -100)
-	}
-	const scrollDown = () => {
-		document.getElementById("row").scrollBy(0, 100)
-	}
-	window.addEventListener("keydown", (e) => {
-		if (e.key === "Escape") {
-			setViewImage(false)
-		}
-	})
+  const scrollUp = () => {
+    document.getElementById("row").scrollBy(0, -100);
+  };
+  const scrollDown = () => {
+    document.getElementById("row").scrollBy(0, 100);
+  };
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      setViewImage(false);
+    }
+  });
 
-	return (
-		<div className="container">
-			{isLoading ? (
-				<p>Loading...</p>
-			) : (
-				<div className="product-page-container">
-					<div className="images-container">
-						<div id="image-row">
-							<button id="scroll-up" onClick={scrollUp}>
-								<VscChevronUp />
-							</button>
-							<div className="row" id="row">
-								{Object.values(images).map((image, key) => (
-									<div
-										id="slide-show-img"
-										className={productPhoto === image ? "active" : ""}
-										key={key}
-										onClick={() => setProductPhoto(image)}
-									>
-										<img src={image} />
-									</div>
-								))}
-							</div>
-							<button id="scroll-down" onClick={scrollDown}>
-								<VscChevronDown />
-							</button>
-						</div>
-						<div className={`${viewImage ? "opened" : ""}`} id="main-image">
-							{viewImage && (
-								<div id="close-image" onClick={() => setViewImage(false)}>
-									<VscChromeClose />
-								</div>
-							)}
+  return (
+    <div className="container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="product-page-container">
+          <div className="images-container">
+            <div id="image-row">
+              <button id="scroll-up" onClick={scrollUp}>
+                <VscChevronUp />
+              </button>
+              <div className="row" id="row">
+                {Object.values(images).map((image, key) => (
+                  <div
+                    id="slide-show-img"
+                    className={productPhoto === image ? "active" : ""}
+                    key={key}
+                    onClick={() => setProductPhoto(image)}
+                  >
+                    <img src={image} />
+                  </div>
+                ))}
+              </div>
+              <button id="scroll-down" onClick={scrollDown}>
+                <VscChevronDown />
+              </button>
+            </div>
+            <div className={`${viewImage ? "opened" : ""}`} id="main-image">
+              {viewImage && (
+                <div id="close-image" onClick={() => setViewImage(false)}>
+                  <VscChromeClose />
+                </div>
+              )}
 
-							<img
-								src={productPhoto}
-								alt={`Image of ${name}`}
-								onClick={() => setViewImage(true)}
-							/>
-						</div>
-					</div>
-					<div className="product-details">
-						<header>
-							<p id="product-name">{name}</p>
-							<p id="price">Rs. {price}</p>
-						</header>
-						<section id="details">
-							<p id="label">Category</p>
-							<p id="value">{category}</p>
-							<p id="label">Colour</p>
-							<p id="value">{color}</p>
-							<p id="label">Fragrance</p>
-							<p id="value">{fragrance}</p>
+              <img
+                src={productPhoto}
+                alt={`Image of ${name}`}
+                onClick={() => setViewImage(true)}
+              />
+            </div>
+          </div>
+          <div className="product-details">
+            <header>
+              <p id="product-name">{name}</p>
+              <p id="price">Rs. {price}</p>
+            </header>
+            <section id="details">
+              <p id="label">Category</p>
+              <p id="value">{category}</p>
+              <p id="label">Colour</p>
+              <p id="value">{color}</p>
+              <p id="label">Fragrance</p>
+              <p id="value">{fragrance}</p>
 
-							<p id="label">Weight</p>
-							<p id="value">
-								{weight} <span>g</span>
-							</p>
-						</section>
-						<section className="buttons">
-							<div id="quantity">
-								<button id="subtract" onClick={handleQuantityChange}>
-									-
-								</button>
-								<span>{quantity}</span>
-								<button id="add" onClick={handleQuantityChange}>
-									+
-								</button>
-							</div>
+              <p id="label">Weight</p>
+              <p id="value">
+                {weight} <span>g</span>
+              </p>
+            </section>
+            <section className="buttons">
+              <div id="quantity">
+                <button id="subtract" onClick={handleQuantityChange}>
+                  -
+                </button>
+                <span>{quantity}</span>
+                <button id="add" onClick={handleQuantityChange}>
+                  +
+                </button>
+              </div>
 
-							<Button
-								id="add-to-cart-button"
-								onClick={addToCart}
-								buttonType="primary"
-							>
-								Add to Cart
-							</Button>
-						</section>
-					</div>
-				</div>
-			)}
+              <Button
+                id="add-to-cart-button"
+                onClick={addToCart}
+                buttonType="primary"
+              >
+                Add to Cart
+              </Button>
+            </section>
 
-			<Alert
-				type="success"
-				message={`Item added to cart. ( quantity : ${quantity} )`}
-				show={show}
-			/>
-		</div>
-	)
-}
+            <div className="extra-info">
+              <p id="extra-info-heading">About the candle making process</p>
 
-export default Product
+              <p id="extra-info-body">
+                Our candles are purely made of soy wax. Each candle contains a
+                lead-free cotton wick, as well as phthalate-free fragrance oils.
+                Our candles are always hand-poured in small batches, ensuring
+                only the highest quality for our customers!
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <Alert
+        type="success"
+        message={`Item added to cart. ( quantity : ${quantity} )`}
+        show={show}
+      />
+    </div>
+  );
+};
+
+export default Product;
 
 //const [productColor, setProductColor] = useState("")
 //const [productScent, setProductScent] = useState("")
 //const [productScentError, setProductScentError] = useState("")
 {
-	/* <div className="colour-list">
+  /* <div className="colour-list">
 								{colors.map((color, key) => (
 									<div className="colour-item-container">
 										<div
@@ -196,7 +214,7 @@ export default Product
 }
 
 {
-	/* <div id="value">
+  /* <div id="value">
 								<Dropdown
 									type="scent"
 									setterFunc={setProductScent}
